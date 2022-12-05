284d9f9044839137c0c8cb269d5f0fc8b5b1496f
atheavycost <maslowik@yandex.ru>
2022-12-05 10:16:06 +0400

    first response to swapi,  asinc func

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Project, based on SWAPI JS
+
+Мы будем использовать Fetch API в этом проекте.
+
+Библиотеки, которые оборачивают API 
+в более высокоуровневые компоненты:
+    -Axios
+    -Superagent
+    -Got
+    -Request
+    -Reqwest
--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import 'bootswatch/dist/slate/bootstrap.min.css'; 
 
-console.log('hello, second project!');
+
+// fetch('https://swapi.dev/api/people/1')
+//     .then((res) => {
+//         // console.log('Got response: ', res.status);
+//         return res.json();
+//     })
+//     .then((body) => {
+//         console.log(body.gender);
+//         console.log(body.name);
+//     });
+
+
+    // перепишем код, используя Асинхронную функцию
+    // это чистый JS,который работает из коробки, без каких-либо библиотек.
+    const getResource = async (url) => {
+        const res = await fetch(url);
+        const body = await res.json();
+        return body;
+    };
+
+    getResource('https://swapi.dev/api/people/1')
+        .then((body) => {
+            console.log(body);
+        });

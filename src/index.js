e57b2353b7f2d67e143aca01cdb66820a82d5a9e
atheavycost <maslowik@yandex.ru>
2022-12-05 10:37:45 +0400

    ловим ошибку сервера

--- src/index.js
+++ src/index.js
@@ -16,11 +16,20 @@ import 'bootswatch/dist/slate/bootstrap.min.css';
     // это чистый JS,который работает из коробки, без каких-либо библиотек.
     const getResource = async (url) => {
         const res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}` + 
+            `, recieved ${res.status}`)
+        }
         const body = await res.json();
         return body;
     };
 
-    getResource('https://swapi.dev/api/people/1')
+    getResource('https://swapi.dev/api/people/65461')
         .then((body) => {
             console.log(body);
+        })
+        .catch((err) => {
+            console.log('Counld not fetch', err);
+            
         });

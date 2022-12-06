c0dd5fb741b3996c68508c61af67da52a6111b79
atheavycost <maslowik@yandex.ru>
2022-12-06 13:55:24 +0400

    вывод ошибки или отрисовка элемента. Компонент-ошибка. Signed-off-by: Maslow Victor <maslowik@yandex.ru>

---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './error-indicator.css';
+import icon from './doggy_3.jpg';
+
+const ErrorIndicator = () => {
+    return (
+        <div className='error-indicator'>
+            <img src={icon} 
+                alt='error icon' 
+                width='150px' 
+                height='150px'/>
+            <span className='boom'>BOOM!</span>
+            <span>something has gone terribly wrong</span>
+            <span>
+                (but we have sent droinds to fix it, already)
+            </span>
+        </div>
+    );
+};
+
+export default ErrorIndicator;
--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 
 import SwapiService from '../../services/swapi-service';
+import ErrorIndicator from '../error-indicator';
 import Spinner from '../spinner';
 
 import './random-planet.css';
@@ -22,55 +23,42 @@ export default class RandomPlanet extends Component {
     onPlanetLoaded = (planet) => {
         this.setState({
             planet,
-            loading: false});
+            loading: false,
+            error: false});
+    };
+
+    onError = (err) => {
+        this.setState({
+            error: true,
+            loading: false
+        })
     };
 
     updatePlanet() {
         const id = Math.floor(Math.random() * 30) + 1;
+        // const id = 13333;
         this.SwapiService
             .getPlanet(id)
-            .then(this.onPlanetLoaded);
+            .then(this.onPlanetLoaded)
+            .catch(this.onError);
     }
 
     render() {
 
-        const { planet, loading } = this.state;
+        const { planet, loading, error } = this.state;
 
-        // if (loading) {
-        //     return <Spinner />
-        // };
+        const hasData = !(loading || error);
 
-        const spinner = loading ? <Spinner /> : null;
-        const content = !loading ? <PlanetView planet={planet} /> : null;
+        const errorMessage = error ? <ErrorIndicator/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = hasData ? <PlanetView planet={planet} /> : null;
 
         return (
             <div className='random-planet jumbotron rounded'>
+                {errorMessage}
                 {spinner}
                 {content}
-
-                {/* <PlanetView planet={planet} /> */}
-                {/* <img className='planet-image'
-                      src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt=' '/>
-                <div>
-                    <h4> {name}</h4>
-                    <ul className='list-group list-group-flush'>
-                        <li className='list-group-item'>
-                            <span className='term'>Population: </span>
-                            <span>{population}</span>
-                        </li>
-                        <li className='list-group-item'>
-                            <span className='term'>Rotation Period:  </span>
-                            <span>{rotationPeriod}</span>
-                        </li>
-                        <li className='list-group-item'>
-                            <span className='term'>Diameter: </span>
-                            <span>{diameter}</span>
-                        </li>
-                    </ul>
-                </div> */}
-
             </div>
-
         );
     };
 }
